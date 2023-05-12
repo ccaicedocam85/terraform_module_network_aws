33cbdb0d4de0d2ea8182f3adf4fa1c3c7d42da00
CRISTIANPC\crist <ccaicedocam@gmail.com>
2023-05-11 20:41:54 -0500

    se corrigue variable

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "creacion_vpc" {
     source = "./network"
     
     name_vpc = "davivienda VPC"
-    cidr_block_ip4 = "10.0.0.0/16"
+    cidr_block_vpc = "10.0.0.0/16"
     cidr_block_sub_net = "10.0.1.0/24"
 
 }
